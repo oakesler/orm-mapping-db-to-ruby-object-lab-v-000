5684904d759ae3f87ece0dffa3141c860dbef9bb
Olivia Kesler <oakesler@protonmail.ch>
2019-12-11 17:33:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,6 +70,8 @@ class Student
   def self.first_X_students_in_grade_10(x)
     sql = "SELECT * FROM students WHERE students.grade = 10 LIMIT ?"
     DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.all_students_in_grade_X(x)
