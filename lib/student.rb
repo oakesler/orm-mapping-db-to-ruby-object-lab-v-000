0f4a525e2b1d5380cdb3317b4a57c97cdeb9ed80
Olivia Kesler <oakesler@protonmail.ch>
2019-12-11 17:26:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ class Student
   end
   
    def self.students_below_12th_grade
-    sql = "SELECT name FROM students WHERE students.grade < 11"
+    sql = "SELECT * FROM students WHERE students.grade < 11"
     DB[:conn].execute(sql)
   end
   
